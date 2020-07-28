86a93c6381f9d7727f51f5d290d25e22e952db73
Joe Njogu <joenjogu97@gmail.com>
2020-07-28 09:31:26 +0300

    Added tweet_scraper.py

---
+++ tweet_scraper.py
@@ -0,0 +1,9 @@
+import tweepy
+
+def get_status_ids(api, search_term, items):
+    status_ids = []
+    
+    for tweet in tweepy.Cursor(api.search,\
+        q= search_term, 
+        ).items(items):
+        status_ids.append(tweet.id)
